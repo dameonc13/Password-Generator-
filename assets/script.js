d44a87cf048aefe1c875f023c2358c50f6237c43
Dameon Charleygit config --global user.email dameoncharley13@hotmail.comgit config --global user.name Dameon <dameoncharley13@hotmail.com>
2019-12-12 21:49:47 -0500

    Sixth Commit

--- assets/script.js
+++ assets/script.js
@@ -7,42 +7,43 @@ var usrinput = prompt("how long do you wnat your password? Must be between 8 and
 var tonum = parseInt(usrinput)
 var insspc = prompt("Do you want special characters?")
 var insnumb = prompt("Do you want number?")
+
 var spcchar = ["/", "^", "$", ".", "|", "?", "*", "+", "(", ")", ".", "<", ">",]
 
 
 
 // btn, when click password is generated
-pw.addEventListener("click", function () {
+pw.addEventListener("click", function(){
     event.preventDefault();
 
-    if (insspc && insnumb == "yes") {
+if (insspc && insnumb == "yes"){
 
-        document.execCommand("pw");
-        randompassword = spcchar[Math.floor(Math.random() * spcchar.length)] + Math.random().toString(36).substring(2, tonum + 1);
-        spcchar[Math.floor(Math.random() * spcchar.length)];
-        var paragraph = document.getElementById("exampleFormControlTextarea1");
-        paragraph.textContent += randompassword;
+    document.execCommand("pw");
+    randompassword = spcchar[Math.floor(Math.random()*spcchar.length)]+ Math.random().toString(36).substring(2, tonum +1) ;
+    spcchar[Math.floor(Math.random()*spcchar.length)];
+    var paragraph = document.getElementById("exampleFormControlTextarea1");
+    paragraph.textContent += randompassword;
 
-
-
-
+   
 
 
 
-    }
 
+
+}
+    
 });
-
+   
 // btn, when click copy the password and reload the page thus deleting the password
-copy.addEventListener("click", function () {
-    event.preventDefault();
-    document.getElementById("exampleFormControlTextarea1").select();
-
-    document.execCommand("Copy");
-
-    alert(" Password copied to clipboard!" + "\n" +
-        " Page will reload and password destroyed");
-
-});
+    copy.addEventListener("click", function(){
+        event.preventDefault();
+        document.getElementById("exampleFormControlTextarea1").select();
+    
+        document.execCommand("Copy");
+    
+        alert(" Password copied to clipboard!"+ "\n"  +
+            " Page will reload and password destroyed");
+    
+    });
 
 console.log(randompassword);
