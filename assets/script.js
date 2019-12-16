16191fa633be344eb265107ab2ece59da0f1303c
Dameon Charleygit config --global user.email dameoncharley13@hotmail.comgit config --global user.name Dameon <dameoncharley13@hotmail.com>
2019-12-16 01:10:47 -0500

    Ninth Commit

--- assets/script.js
+++ assets/script.js
@@ -2,48 +2,93 @@
 var decrementEl = document.querySelector("#copy");
 var decrementEl = document.querySelector("#pw");
 var randompassword = "";
+var random = "";
+var randompw = "";
 
 var usrinput = prompt("how long do you wnat your password? Must be between 8 and 128 characters?")
-var tonum = parseInt(usrinput)
+var tonum = parseInt(usrinput - 1)
+var insupper = prompt("Do you want uppercase characters?")
+var inslower = prompt("Do you want lowercase characters?")
 var insspc = prompt("Do you want special characters?")
 var insnumb = prompt("Do you want number?")
 
-var spcchar = ["/", "^", "$", ".", "|", "?", "*", "+", "(", ")", ".", "<", ">",]
+var spcchar = "/^$.,|?*+().<>"
+var capschar = "ABCDEFGHIJKLMNOPQRSTUVWZYZ"
+var lowerchar = "abcdefghijklmnopqrstuvwxyz"
+var numb = "1234567890"
+var password = "";
+
+function generatepassword() {
+    if (inslower == "yes") {
+        for (var i = 0; i <= tonum / 2; i++) {
+
+            password = password + lowerchar.charAt(Math.floor(Math.random() * Math.floor(lowerchar.length)));
+
+        }
+    }
+
+    if (insupper == "yes") {
+        for (var i = 0; i <= tonum / 2; i++) {
+
+            randompassword = randompassword + capschar.charAt(Math.floor(Math.random() * Math.floor(capschar.length)));
+
+        }
+    }
+
+    if (insspc == "yes") {
+        for (var i = 0; i <= tonum / 2; i++) {
+
+            random = random + spcchar.charAt(Math.floor(Math.random() * Math.floor(spcchar.length)));
+
+        }
+    }
+
+    if (insnumb == "yes") {
+        for (var i = 0; i <= tonum / 2; i++) {
+
+            randompw = randompw + numb.charAt(Math.floor(Math.random() * Math.floor(numb.length)));
 
 
+        }
+    }
 
-// btn, when click password is generated
-pw.addEventListener("click", function(){
-    event.preventDefault();
 
-if (insspc && insnumb == "yes"){
+    var genpw = "";
+    var pass = "";
+    genpw = password + randompassword + randompw + random
+    for (var i = 0; i <= tonum; i++) {
+        pass = pass + genpw.charAt(Math.floor(Math.random() * Math.floor(genpw.length)));
+    }
 
-    document.execCommand("pw");
-    randompassword = spcchar[Math.floor(Math.random()*spcchar.length)]+ Math.random().toString(36).substring(2, tonum +1) ;
-    spcchar[Math.floor(Math.random()*spcchar.length)];
+
     var paragraph = document.getElementById("exampleFormControlTextarea1");
-    paragraph.textContent = randompassword;
-
-   
-
+    paragraph.textContent = pass;
 
 
 
 
 }
-    
-});
-   
-// btn, when click copy the password and reload the page thus deleting the password
-    copy.addEventListener("click", function(){
-        event.preventDefault();
-        document.getElementById("exampleFormControlTextarea1").select();
-    
-        document.execCommand("Copy");
-    
-        alert(" Password copied to clipboard!"+ "\n"  +
-            " Page will reload and password destroyed");
-    
-    });
 
-console.log(randompassword);
+
+
+
+
+// btn, when click password is generated
+pw.addEventListener("click", (event) => {
+
+    event.preventDefault();
+    generatepassword();
+
+});
+
+// btn, when click copy the password and reload the page thus deleting the password
+copy.addEventListener("click", function () {
+    event.preventDefault();
+    document.getElementById("exampleFormControlTextarea1").select();
+
+    document.execCommand("Copy");
+
+    alert(" Password copied to clipboard!");
+
+});
+
