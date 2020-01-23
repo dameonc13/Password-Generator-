57616d8bf66207b60599481e87a0db6d432fe076
Dameon Charleygit config --global user.email dameoncharley13@hotmail.comgit config --global user.name Dameon <dameoncharley13@hotmail.com>
2020-01-23 06:36:18 -0500

    force user to enter character limit  no less than 8

--- assets/script.js
+++ assets/script.js
@@ -6,12 +6,27 @@ var random = "";
 var randompw = "";
 
 var usrinput = prompt("how long do you wnat your password? Must be between 8 and 128 characters?")
-var tonum = parseInt(usrinput - 1)
-var insupper = prompt("Do you want uppercase characters?")
-var inslower = prompt("Do you want lowercase characters?")
-var insspc = prompt("Do you want special characters?")
-var insnumb = prompt("Do you want number?")
 
+for (i = 7; i < 8; i++) {
+if (usrinput < 8 || usrinput == "" ){
+
+    usrinput = prompt("Password character must be  8 or greater")
+    var tonum = parseInt(usrinput - 1)
+     i = tonum;
+
+}
+}
+
+var tonum = parseInt(usrinput - 1)
+var insUpper = prompt("Do you want uppercase characters?")
+var insLower = prompt("Do you want lowercase characters?")
+var insSpc = prompt("Do you want special characters?")
+var insNumb = prompt("Do you want number?")
+
+/*if (insUpper || insLower || insSpc || insNumb == "" ){
+
+    
+}  */
 var spcchar = "/^$.,|?*+().<>"
 var capschar = "ABCDEFGHIJKLMNOPQRSTUVWZYZ"
 var lowerchar = "abcdefghijklmnopqrstuvwxyz"
@@ -21,7 +36,14 @@ var password = "";
 
 // Function generates password based on user preference 
 function generatepassword() {
-    if (inslower == "yes") {
+    
+    
+    
+    
+    
+    if (insLower == "yes") {
+      
+        
         for (var i = 0; i <= tonum / 2; i++) {
 
             password = password + lowerchar.charAt(Math.floor(Math.random() * Math.floor(lowerchar.length)));
@@ -29,7 +51,7 @@ function generatepassword() {
         }
     }
 
-    if (insupper == "yes") {
+    if (insUpper == "yes") {
         for (var i = 0; i <= tonum / 2; i++) {
 
             randompassword = randompassword + capschar.charAt(Math.floor(Math.random() * Math.floor(capschar.length)));
@@ -37,7 +59,7 @@ function generatepassword() {
         }
     }
 
-    if (insspc == "yes") {
+    if (insSpc == "yes") {
         for (var i = 0; i <= tonum / 2; i++) {
 
             random = random + spcchar.charAt(Math.floor(Math.random() * Math.floor(spcchar.length)));
@@ -45,7 +67,7 @@ function generatepassword() {
         }
     }
 
-    if (insnumb == "yes") {
+    if (insNumb == "yes") {
         for (var i = 0; i <= tonum / 2; i++) {
 
             randompw = randompw + numb.charAt(Math.floor(Math.random() * Math.floor(numb.length)));
@@ -53,6 +75,8 @@ function generatepassword() {
 
         }
     }
+
+    
 
 // takes all the characters and randomize it then writes to the document 
     var genpw = "";
